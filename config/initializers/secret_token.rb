51dce4c053620d8f7fb6fcd18539eba0ccd4182d
André Rieck <4ndr3r13ck@googlemail.com>
2012-11-03 15:01:18 +0100

    setup rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StressMaps::Application.config.secret_token = '339301232e57a90aadc447077174b1c50e1c3f291bc71414ca2e653c5ad4064c9f0153ecf2e96d17174085ee09b3c77468d204903ba061f9dab83c1b688dfd0b'
